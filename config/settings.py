27f0863891e834b6c74a4dc501268ab7eb761619
EngRobot33 <hmdkhsrvee@gmail.com>
2024-01-23 23:07:38 +0330

    [CHORE] settings environment variables

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='this-is-the-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASS"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
+        "NAME": config("DB_NAME", default='ip-country-lookup'),
+        "USER": config("DB_USER", default='anonymous'),
+        "PASSWORD": config("DB_PASS", default='anonymous'),
+        "HOST": config("DB_HOST", default='db'),
+        "PORT": config("DB_PORT", default='5432'),
     }
 }
 
